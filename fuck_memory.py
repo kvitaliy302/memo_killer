f97ceb531e21bf5c7c894b9d9ce87ec0c0ed0ed1
kvitaliy302 <kvitaliy302@gmail.com>
2021-09-21 17:48:08 +0300

    asdas

--- fuck_memory.py
+++ fuck_memory.py
@@ -1,3 +1,7 @@
-memo_killer = ['#####################################################################################################']
-while True:
-    memo_killer.append(memo_killer)
+import traceback
+try:
+    memo_killer = ['#####################################################################################################']
+    while True:
+        memo_killer.append(memo_killer)
+except:
+    print(traceback.format_exc())
